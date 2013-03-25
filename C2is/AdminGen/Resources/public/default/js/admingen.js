4729ef630da7e9a03376b9a61a9c19aceb33257c
Morgan Brunot <brunot.morgan@gmail.com>
2013-03-25 11:04:19 +0100

    improve interface

--- C2is/AdminGen/Resources/public/default/js/admingen.js
+++ C2is/AdminGen/Resources/public/default/js/admingen.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-    $('.component .form-btn-submit').bind('click', function (e) {
+    $(".component .form-btn-submit").bind("click", function (e) {
         var base = $(this);
 
         var contentComponent = base
-            .parent('.component-footer')
-            .parent('.component')
-            .children('.component-content');
+            .parent(".component-footer")
+            .parent(".component")
+            .children(".component-content");
 
-        $('form', contentComponent).submit();
+        $("form", contentComponent).submit();
     });
 
-    $('.actions-item .delete').confirmModal();
+    $(".actions-item .delete").confirmModal();
 
     $("*[required]").not("[type=submit]").jqBootstrapValidation();
+
+    $("select").width($("select").width()).select2();
 });
